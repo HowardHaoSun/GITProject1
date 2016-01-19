af2d181574bb78730e8195240298109e6bc0835b
HowardSunHao <sunhaocmdi@126.com>
2016-01-19 22:29:19 +0800

    增加注释

--- SportsStore/App_Start/BundleConfig.cs
+++ SportsStore/App_Start/BundleConfig.cs
@@ -3,6 +3,7 @@ using System.Web.Optimization;
 
 namespace SportsStore
 {
+    //添加一行注释试试
     public class BundleConfig
     {
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
