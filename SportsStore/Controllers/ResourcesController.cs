c7d72b0842cc36c29f16651f89148203f561aced
HowardHaoSun <sunhaocmdi@126.com>
2016-01-20 11:08:09 +0800

    增加国际化API，ResourcesController

---
+++ SportsStore/Controllers/ResourcesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace SportsStore.Controllers
+{
+    [Route("api/resources/{name}/{culture}")]
+    public class ResourcesController : ApiController
+    {
+        public string GetCulture(string name, string culture)
+        {
+            CultureInfo curUICulture = CultureInfo.CurrentUICulture;
+            CultureInfo curCulture = CultureInfo.CurrentCulture;
+            try
+            {
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+                return SportsStore.Properties.Resources.ResourceManager.GetString(name.ToLower());
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentUICulture = curUICulture;
+                Thread.CurrentThread.CurrentCulture = curCulture;
+            }
+        }
+    }
+}
